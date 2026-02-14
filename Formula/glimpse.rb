5f1e5ec533003d8edf533edde4299335b87bfbf1
Christoph Kissmann <72171282+kissmannchristoph@users.noreply.github.com>
2026-02-14 04:45:49 +0100

    fix(glimpse): fix

--- Formula/glimpse.rb
+++ Formula/glimpse.rb
@@ -1,6 +1,7 @@
 class Glimpse < Formula
   desc "Deine App Beschreibung"
   homepage "https://github.com/ckissmann/glimpse"
+  version "2026.2.14.14"
   url "https://github.com/ckissmann/glimpse/archive/refs/tags/v2026.2.14.14.tar.gz"
   sha256 "143f478c029b1534fa56cb7b69b1da6d274ea8498d231222de99e4132eb94844"
   license "MIT"
